38b45056aad9e6dfa8cce867afb201d91373c154
James Stern <jamesstern18@gmail.com>
2014-08-30 11:26:50 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Secondhandstoke::Application.config.secret_key_base = '125533bd434373af0084f27fd6235fd33d8021e2702deb020541992814467f0972db6b64ba37bf40666a7d566d1d7f74baadde616472c47080c60c8f2af31512'
