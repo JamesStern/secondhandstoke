d9324b426d09b5bd73c81e36dad80cac46202f58
James Stern <jamesstern18@gmail.com>
2014-08-30 13:10:46 -0700

    Update home title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,14 @@ describe "StaticPages" do
   		expect(page).to have_content('Secondhand Stoke')
   	end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Secondhand Stoke | Home")
+      expect(page).to have_title("Secondhand Stoke")
+    end
+
+    it "should not have a custome page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
